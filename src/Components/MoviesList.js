ef2340164469573314b562ff9c75afd38eb751ae
shubham <sawantshubham736@gmail.com>
2025-12-25 16:33:56 +0530

    ready for vercel deployment

--- src/Components/MoviesList.js
+++ src/Components/MoviesList.js
@@ -1,14 +1,16 @@
 import MovieCard from "./MovieCard";
 
 const MoviesList = ({ title, data }) => {
-  if (!data) return;
+  if (!data) return null;
+
   return (
     <div className="pb-5">
       <h1 className="font-bold text-3xl px-3 py-3">{title}</h1>
-      <div className="flex overflow-x-auto scrollbar-hide">
+
+      <div className="overflow-x-scroll custom-scroll scroll-smooth">
         <div className="flex gap-8 p-3">
           {data.map((movie) => (
-            <MovieCard data={movie} key={movie.id} />
+            <MovieCard key={movie.id} data={movie} />
           ))}
         </div>
       </div>
